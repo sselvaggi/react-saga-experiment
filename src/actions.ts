632b8dec507c590fc4cb3929d7e959d3a38f1fc8
Sergio Selvaggi <sergio.selvaggi@gmail.com>
2020-07-06 07:12:20 +0200

    ADDED product actions [partial commit]

--- src/actions.ts
+++ src/actions.ts
@@ -1,5 +1,17 @@
 export const REQUEST_API_DATA = "REQUEST_API_DATA";
 export const RECEIVE_API_DATA = "RECEIVE_API_DATA";
+export const FETCH_PRODUCTS_REQUEST = 'FETCH_PRODUCTS_REQUEST';
+export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
+export const FETCH_PRODUCTS_FAILURE = 'FETCH_PRODUCTS_FAILURE';
+export const SAVE_PRODUCT_REQUEST = 'SAVE_PRODUCT_REQUEST';
+export const SAVE_PRODUCT_SUCCESS = 'SAVE_PRODUCT_SUCCESS';
+export const SAVE_PRODUCT_FAILURE = 'SAVE_PRODUCT_FAILURE';
 
 export const requestApiData = () => ({ type: REQUEST_API_DATA });
 export const receiveApiData = data => ({ type: RECEIVE_API_DATA, data });
+export const fetchProductRequest = (data: ProductQuery | null) => ({ type: FETCH_PRODUCTS_REQUEST, data });
+export const fetchProductSuccess = (data: Product[]) => ({ type: FETCH_PRODUCTS_SUCCESS, data });
+export const fetchProductFailure = (data:string) => ({ type: FETCH_PRODUCTS_FAILURE, data });
+export const saveProductRequest = (data:Product) => ({ type: SAVE_PRODUCT_REQUEST, data });
+export const saveProductSuccess = (data:Product) => ({ type: SAVE_PRODUCT_SUCCESS, data });
+export const saveProductFailure = (data:string) => ({ type: SAVE_PRODUCT_FAILURE, data });
--- src/reducers/data.ts
+++ src/reducers/data.ts
@@ -1,10 +1,80 @@
-import { RECEIVE_API_DATA } from "../actions";
+import * as ACTIONS from "../actions";
+import Product from './Product'
 
-export default (state = {}, { type, data }) => {
+export interface Action {
+  type: string,
+  data: any,
+}
+export default (state = {}, { type, data }:Action) => {
   switch (type) {
-    case RECEIVE_API_DATA:
+    case ACTIONS.RECEIVE_API_DATA:
       return data;
+    case ACTIONS.FETCH_PRODUCTS_REQUEST:
+      return {
+        ...state,
+        fetchingProductQuery: true,
+        fetchingProductError: null,
+      };
+    case ACTIONS.FETCH_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        fetchingProductQuery: null,
+        fetchingProductError: null,
+        products: data,
+      };
+    case ACTIONS.FETCH_PRODUCTS_FAILURE:
+      return {
+        ...state,
+        fetchingProductQuery: null,
+        fetchingProductError: data,
+      };
+    case ACTIONS.SAVE_PRODUCT_REQUEST:
+      return {
+        ...state,
+        savingProductRequest: true,
+        savingProductError: null,
+      };
+    case ACTIONS.SAVE_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        savingProductRequest: null,
+        savingProductError: null,
+      };
+    case ACTIONS.SAVE_PRODUCT_FAILURE:
+      return {
+        ...state,
+        savingProductRequest: null,
+        creatingProductError: data,
+      };
     default:
       return state;
   }
+
 };
+export interface RootState {
+  fetchingProductRequest: Action | null,
+  fetchingProductError: string | null,
+  savingProductRequest: Action | null,
+  savingPruductError: string | null,
+  deletingProductRequest: Action | null,
+  deletingPruductError: string | null,
+  products: Product[]
+}
+const initState:RootState = {
+  fetchingProductRequest: null,
+  fetchingProductError: null,
+  savingProductRequest: null,
+  savingPruductError: null,
+  deletingProductRequest: null,
+  deletingPruductError: null,
+  products: [
+    {
+      name:"pepo",
+      price: 20,
+    },
+    {
+      name:"pepa", 
+      price: 27,
+    }
+  ]
+}
